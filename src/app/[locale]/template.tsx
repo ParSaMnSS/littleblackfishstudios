e9f686b196960f331e497d61253dafc0d948d023
Parsa <parsaxavier@gmail.com>
2026-02-24 11:59:39 +0300

    page transitions

--- src/app/[locale]/template.tsx
+++ src/app/[locale]/template.tsx
@@ -5,10 +5,9 @@ import { motion } from 'framer-motion';
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.main
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -20 }}
-      transition={{ ease: 'easeInOut', duration: 0.5 }}
+      transition={{ ease: 'easeOut', duration: 0.3 }}
     >
       {children}
     </motion.main>
